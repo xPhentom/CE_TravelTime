1ab7eb42436aec89d887be75165ecf516c18b8b3
Wouter Roozeleer <phentom@live.be>
2016-12-05 17:30:15 +0100

    Small UI change, trying to work with leaflet through Angular

--- static/script.js
+++ static/script.js
@@ -2,20 +2,28 @@ var app = angular.module("homeApp", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
     $routeProvider
+    //Return index template
         .when("/", {
-            templateUrl: "/start.html",
-            controller: "startCtrl"
-        })
+        templateUrl: "/start.html",
+        controller: "startCtrl"
+    })
 })
 
-app.controller('startCtrl', ['$scope', function ($scope) {
+
+
+//Indexcontroller
+app.controller('startCtrl', ['$scope', function ($scope, $http) {
+
+    //Create map, can be found back on leaflet website
     var map = new L.Map('mapid', {
         center: new L.LatLng(51.51, -0.11),
         zoom: 9
     });
     var googleLayer = new L.Google('ROADMAP');
     map.addLayer(googleLayer);
+    
 
+    //Create noUiSlider, doesn't come with materialize.css
     var stepSlider = document.getElementById('TimeSlider');
 
     noUiSlider.create(stepSlider, {
@@ -26,11 +34,23 @@ app.controller('startCtrl', ['$scope', function ($scope) {
             'max': [60]
         }
     });
-
+    //Get value from Slider
     var stepSliderValueElement = document.getElementById('TimeSliderValue');
 
     stepSlider.noUiSlider.on('update', function (values, handle) {
         stepSliderValueElement.innerHTML = values[handle];
     });
+
+    $scope.MapClick = function(e){
+
+        var search = (e.latlng.lat + ", " + e.latlng.lng);
+
+        $http({
+            method: "POST",
+            url: "http://nominatim.openstreetmap.org/search?q=" + search + "&format=json&polygon=0&addressdetails=1&extratags=1",
+        }).then(function mySuccess(response){
+
+        }), function myError(response){}
+    }
 
 }])
